3752cecb1f7e8e7486e5fcbf34381d9f7b85e030
root <root@ip-172-31-84-202.ec2.internal>
2020-08-16 11:48:35 +0000

    cognito

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_OfLPpLakR',
         userPoolClientId: '2eb9mg5t9kq234i42nuetnndm6',
-        region: 'us-east-1',
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
